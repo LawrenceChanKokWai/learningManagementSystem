cf70ac3cb750d524948958217482b47d53f5fef7
Lawrence <68629118+LawrenceChanKokWai@users.noreply.github.com>
2021-11-04 01:58:14 +0800

    wrapper component user route completed

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -13,7 +13,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 
-const { Item, SubMenu } = Menu;
+const { Item, SubMenu, ItemGroup } = Menu;
 
 const TopNav = () => {
 	const [current, setCurrent] = useState("");
@@ -99,9 +99,14 @@ const TopNav = () => {
 					title={user && user.name}
 					className="float-right"
 				>
-					<Item onClick={logout} className="float-right">
-						Logout
-					</Item>
+					<ItemGroup>
+						<Item key="/user">
+							<Link href="/user">
+								<a>Dashboard</a>
+							</Link>
+						</Item>
+						<Item onClick={logout}>Logout</Item>
+					</ItemGroup>
 				</SubMenu>
 			)}
 		</Menu>
